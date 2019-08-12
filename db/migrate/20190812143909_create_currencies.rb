33d3394f4871afd53d1d4bdb06dbfe44ce65913f
Nazrul Babu <n95babu@gmail.com>
2019-08-12 11:01:36 -0400

    Add DataBase Model

---
+++ db/migrate/20190812143909_create_currencies.rb
@@ -0,0 +1,11 @@
+class CreateCurrencies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :currencies do |t|
+      t.string :coin
+      t.float :amount
+      t.references :User, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
