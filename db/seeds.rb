116944b2071d19dc1c58a42f035d649be8f685eb
Nazrul Babu <n95babu@gmail.com>
2019-08-12 15:08:13 -0400

    User Controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create(
+  first_name: "Ivonka",
+  last_name: "Trump",
+  email: "itrump@vallet.com",
+  phone: "917-678-5630"
+)
+
+currency1 = Currency.create(
+  coin: "Bitcoin",
+  amount: 40.50, 
+)
+
