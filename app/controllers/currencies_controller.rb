24c93aefc134cb788595f857101f6023465721c3
Nazrul Babu <n95babu@gmail.com>
2019-08-13 12:43:55 -0400

    Fix Bug

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,3 +1,44 @@
 class CurrenciesController < ApplicationController
-  
+  def index
+    @user = User.find(params[:user_id])
+    @currencies = Currency.where(user_id: @user.id)
+    render json: @currencies, include: :user, status: :ok
+  end
+
+  def show 
+    @user = User.find(params[:user_id])
+    @currency = Currency.find(params[:id])
+    render json: @currency, include: :user, status: :ok
+  end
+
+  def create
+    # @user = User.find(params[:user_id])
+    @currency = Currency.new(currencies_params)
+    if @currency.save
+      render json: @currency, status: :created
+    else
+      render json: @currency.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @currency = Currency.find(params[:id])
+    if @currency.update(currency_params)
+      render json: @currency, status: :ok
+    else
+      render json: { errors: @currency.errors }, status: :unprocessable_entity
+    end
+  end
+
+   def destroy
+    @currency = Currency.find(params[:id])
+    @currency.destroy
+    # come back to this
+  end
+
+   private
+   
+   def currency_params
+    params.require(:currency).permit(:coin, :amount, :user_id)
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   resources :users do
     resources :currencies
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
