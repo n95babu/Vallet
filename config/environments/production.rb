d56e3a81659aa6760563bcea40b62cb2a172bdf8
Nazrul Babu <n95babu@gmail.com>
2019-10-20 12:06:31 -0400

    Action Mailer Bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,22 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+
+# SMTP settings for gmail
+config.action_mailer.smtp_settings = {
+ :address              => "smtp.gmail.com",
+ :port                 => 587,
+ :user_name            => 'valletexc@gmail.com',
+ :password             => 'Macadmin1',
+ :authentication       => "plain",
+:enable_starttls_auto => true
+}
+
+
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
