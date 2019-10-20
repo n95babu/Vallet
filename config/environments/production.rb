d1494ce6d780027bb7907fab43672599f3fa6cd3
Nazrul Babu <n95babu@gmail.com>
2019-10-20 12:36:00 -0400

    Action Mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,10 +70,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-
 # SMTP settings for gmail
 config.action_mailer.smtp_settings = {
  :address              => "smtp.gmail.com",
